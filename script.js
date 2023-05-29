0617dfb6f72978c0a1f4b63e0b8d14981155e374
Mark Abraham Goldstein <123721646+markgoldstein98@users.noreply.github.com>
2023-05-29 10:58:43 +0200

    modified fetch

--- script.js
+++ script.js
@@ -2,28 +2,17 @@ const API_KEY = "090d863f-b62e-41f4-9347-ff4a11ed909c";
 const root = document.querySelector("#root");
 const CRYPTO_SYMBOL = "BTC";
 
-/* const apiUrl = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?symbol=${CRYPTO_SYMBOL}&CMC_PRO_API_KEY=${API_KEY}`; */
+const apiUrl = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?symbol=${CRYPTO_SYMBOL}&CMC_PRO_API_KEY=${API_KEY}`;
 
-async function getFetch(apiUrl) {
-  const response = await fetch(apiUrl);
+async function getFetch(url) {
+  const response = await fetch(url);
   const data = await response.json();
 
   return data;
 }
-/* console.log(getFetch()); */
 
-async function log() {
-  console.log(
-    await getFetch(
-      `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${API_KEY}`
-    )
-  );
-}
-log();
-
-/* function getCoin() {
-  let a = async function () {
-    let data = await getFetch(apiUrl);
-  };
-}
- */
+console.log(
+  getFetch(
+    `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?symbol=${CRYPTO_SYMBOL}&CMC_PRO_API_KEY=${API_KEY}`
+  )
+);
