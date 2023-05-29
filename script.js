43b4b3704acf0f9ece34b541d59ee0525ff79be6
Mark Abraham Goldstein <123721646+markgoldstein98@users.noreply.github.com>
2023-05-29 17:41:57 +0200

    changed the fetch

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const API_KEY = "090d863f-b62e-41f4-9347-ff4a11ed909c";
 const root = document.querySelector("#root");
 const CRYPTO_SYMBOL = "BTC";
 
-const apiUrl = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?symbol=${CRYPTO_SYMBOL}&CMC_PRO_API_KEY=${API_KEY}`;
-
-async function getFetch(url) {
-  const response = await fetch(url);
+async function getFetch(url, headers) {
+  const response = await fetch(url, headers);
+  {
+    headers: {
+      Authorization: API_KEY;
+    }
+  }
   const data = await response.json();
 
   return data;
@@ -13,6 +16,6 @@ async function getFetch(url) {
 
 console.log(
   getFetch(
-    `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?symbol=${CRYPTO_SYMBOL}&CMC_PRO_API_KEY=${API_KEY}`
+    `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${API_KEY}`
   )
 );
